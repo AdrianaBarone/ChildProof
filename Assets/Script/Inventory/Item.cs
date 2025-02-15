687b798f96ffebcdce16e04ea72f76c71c222a7e
NiccoBram <158487655+NiccoBram@users.noreply.github.com>
2025-02-15 11:17:59 +0100

    Cambio script Item

--- Assets/Script/Inventory/Item.cs
+++ Assets/Script/Inventory/Item.cs
@@ -1,12 +1,48 @@
+using System.Diagnostics;
 using UnityEngine;
 
 
 public class Item : MonoBehaviour, IPickable {
     public ItemData data;
+    [Header("Audio Animazione")]
+    private AudioClip[] audioClips;
+    public AudioSource[] audioSources;
+
+    private void Start()
+    {
+        // Rimuove AudioSource esistenti per evitarne la duplicazione
+        foreach (var source in GetComponents<AudioSource>())
+        {
+            Destroy(source);
+        }
+
+        audioSources = new AudioSource[audioClips.Length];
+
+        for (int i = 0; i < audioClips.Length; i++)
+        {
+            audioSources[i] = gameObject.AddComponent<AudioSource>();
+            audioSources[i].clip = audioClips[i];
+        }
+
+        if (audioClips.Length == 0)
+        {
+            Debug.LogWarning("Nessun audio clip assegnato.");
+        }
+    }
 
     public void OnPick() {
         InventoryManager.Instance.Add(this);
         // NOTE: non distruggo l'oggetto dato che serve il riferimento per l'inventario
         gameObject.SetActive(false);
+    }
+    public void PlayAudioOnAnimation()
+    {
+        foreach (var source in audioSources)
+        {
+            if (source.clip != null && !source.isPlaying)
+            {
+                source.Play();
+            }
+        }
     }
 }

94e7ae351429b64e75073536206194fa1c21a2dd
antonio-campobasso <campobassoantonio7@gmail.com>
2025-02-18 22:31:30 +0100

    formattato

--- Assets/QuickOutline/Scripts/HoverOutline.cs
+++ Assets/QuickOutline/Scripts/HoverOutline.cs
@@ -15,53 +15,55 @@ public class HoverOutline : MonoBehaviour
     {
         // Recupera il componente Outline e lo disabilita all'avvio
         outline = GetComponent<Outline>();
-        if (outline != null){
-            outline.enabled = false;
-        outline.precomputeOutline = false;
-    }
-
-    void OnMouseEnter()
-    {
-        mouseOver = true;
-        // Attiva l'outline solo se l'oggetto è entro la distanza desiderata
-        if (IsWithinDistance())
-        {
-            outline.enabled = true;
-        }
-    }
-
-    void OnMouseExit()
-    {
-        mouseOver = false;
-        // Disabilita l'outline quando il mouse esce
         if (outline != null)
-            outline.enabled = false;
-    }
-
-    void Update()
-    {
-        // Se il mouse è sopra l'oggetto, controlla continuamente la distanza dalla camera
-        if (mouseOver && PlayerManager.Instance.IsInStateExploration())
         {
+            outline.enabled = false;
+            outline.precomputeOutline = false;
+        }
+
+        void OnMouseEnter()
+        {
+            mouseOver = true;
+            // Attiva l'outline solo se l'oggetto è entro la distanza desiderata
             if (IsWithinDistance())
             {
-                if (!outline.enabled)
-                    outline.enabled = true;
-            }
-            else
-            {
-                if (outline.enabled)
-                    outline.enabled = false;
+                outline.enabled = true;
             }
         }
-    }
 
-    bool IsWithinDistance()
-    {
-        if (Camera.main == null)
-            return false;
+        void OnMouseExit()
+        {
+            mouseOver = false;
+            // Disabilita l'outline quando il mouse esce
+            if (outline != null)
+                outline.enabled = false;
+        }
 
-        float distance = Vector3.Distance(Camera.main.transform.position, transform.position);
-        return distance <= maxDistance;
+        void Update()
+        {
+            // Se il mouse è sopra l'oggetto, controlla continuamente la distanza dalla camera
+            if (mouseOver && PlayerManager.Instance.IsInStateExploration())
+            {
+                if (IsWithinDistance())
+                {
+                    if (!outline.enabled)
+                        outline.enabled = true;
+                }
+                else
+                {
+                    if (outline.enabled)
+                        outline.enabled = false;
+                }
+            }
+        }
+
+        bool IsWithinDistance()
+        {
+            if (Camera.main == null)
+                return false;
+
+            float distance = Vector3.Distance(Camera.main.transform.position, transform.position);
+            return distance <= maxDistance;
+        }
     }
 }

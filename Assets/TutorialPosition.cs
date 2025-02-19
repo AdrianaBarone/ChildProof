5701fb035b4ea76ef8dc585f8facfb67850f7638
Adriana <adrianabarone.ad00@gmail.com>
2025-02-19 12:55:26 +0100

    primo commit tutorial

--- Assets/TutorialPosition.cs
+++ Assets/TutorialPosition.cs
@@ -5,7 +5,6 @@ public class TutorialPosition : MonoBehaviour
     // when  colliding with something send event to TutorialManager
     void OnTriggerEnter(Collider triggerColliderEnter)
     {
-        Debug.Log("AAAAA");
         TutorialManager.Instance.OnReceiveEvent("RaggiungiScaffale");
     }
 

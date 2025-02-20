68a0fa865d1300646e61e71b3c150c43fc70e370
antonio-campobasso <campobassoantonio7@gmail.com>
2025-02-20 01:35:55 +0100

    prima build

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 
-public class TutorialManager : MonoBehaviour {
+public class TutorialManager : MonoBehaviour
+{
 
     public static TutorialManager Instance;
     [SerializeField] private string nextSceneName;
@@ -11,41 +11,50 @@ public class TutorialManager : MonoBehaviour {
     [SerializeField] private GameObject inventoryCanvas;
     private int currentStep = 0;
 
-    private void Awake() {
+    private void Awake()
+    {
         Instance = this;
     }
 
-    private void Start() {
+    private void Start()
+    {
         OnReceiveEvent("Inizio");
     }
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             inventoryCanvas.SetActive(true);
             sequences[currentStep - 1].tutorialPanel.Hide();
         }
     }
 
-    public void OnReceiveEvent(string eventKey) {
-        if (eventKey == sequences[currentStep].eventKey) {
-            if (currentStep == sequences.Length - 1) {
+    public void OnReceiveEvent(string eventKey)
+    {
+        if (eventKey == sequences[currentStep].eventKey)
+        {
+            if (currentStep == sequences.Length - 1)
+            {
                 SceneManager.LoadSceneAsync(nextSceneName);
                 return;
             }
 
-            
-            if (sequences[currentStep].tutorialPanel != null)  {
+
+            if (sequences[currentStep].tutorialPanel != null)
+            {
                 sequences[currentStep].tutorialPanel.Show();
                 inventoryCanvas.SetActive(false);
             }
-                
+
             currentStep++;
         }
     }
 }
 
 [System.Serializable]
-public struct Sequence {
+public struct Sequence
+{
     public string eventKey;
     public TutorialPanel tutorialPanel;
 }
